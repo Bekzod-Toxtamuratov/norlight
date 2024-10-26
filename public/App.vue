ca71f3d831757e5dc5c1e9e6e3fa6e270e156df4
Bekzod-Toxtamuratov <155290650+Bekzod-Toxtamuratov@users.noreply.github.com>
2024-10-26 20:41:06 +0500

    Add files via upload

---
+++ public/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import Button from "./components/Button.vue"
+
+
+</script>
+
+<template>
+	<header>
+		<h2>Bekzod-Toxtamuratov</h2>
+    <Button text="Bosh Sayt" />
+	</header>
+
+	<main></main>
+</template>
+
+<style scoped></style>
